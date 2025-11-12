7b008997ac5838c579fea53141d92b2c263ca5c8
Malthe <grom1020@gmail.com>
2025-11-12 15:44:17 +0100

    moved project structure back to original, as server part will now be a separate process

--- src/extension.ts
+++ src/extension.ts
@@ -20,8 +20,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, the extension "choral-syntax-extension" is now active!');
 
-	const serverModule: string = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-	const serverOptions: ServerOptions = {
+	// Refer to: https://github.com/tempo-lang/vscode-tempo/blob/main/src/extension.ts
+
+	const serverModule: string = context.asAbsolutePath(path.join('server', 'out', 'server.js')); // this needs revising once proper path has been figured out
+
+	const serverOptions: ServerOptions = { // these options need to be revisied to run a java program somehow
+		// likely need to add actual options specific to the Choral LSP "server" once those have been implemented
 		run: { module: serverModule, transport: TransportKind.ipc },
 		debug: {
 			module: serverModule,
@@ -29,6 +33,7 @@ export function activate(context: vscode.ExtensionContext) {
 			options: { execArgv: ['--nolazy', '--inspect=6009'] }
 		}
 	};
+
 	const clientOptions: LanguageClientOptions = {
 		documentSelector: [{ scheme: 'file', language: 'choral' }],
 		synchronize: {
